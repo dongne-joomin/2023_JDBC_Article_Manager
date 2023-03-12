d43c8b5b797c7a1b5d1007c3c38a8f7c27f3c116
dongne-joomin <donghunc88@gmail.com>
2023-03-13 02:45:00 +0900

    JDBC Select Test

--- src/com/koreaIT/example/JAM/Main.java
+++ src/com/koreaIT/example/JAM/Main.java
@@ -1,5 +1,9 @@
 package com.KoreaIT.example.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,28 +14,63 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		List<Article> articles = new ArrayList<>();
-
 		int lastArticleId = 0;
 
 		while (true) {
 			System.out.printf("명령어)");
 			String cmd = sc.nextLine().trim();
 
-			if(cmd.equals("article write")) {
+			if (cmd.equals("article write")) {
 				System.out.println("== 게시물 작성 ==");
-				
+
 				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
-				Article article = new Article(id, title, body);
-				
-				articles.add(article);
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/jdbc_article_manager";
+
+					conn = DriverManager.getConnection(url, "root", "");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = '" + title + "'";
+					sql += ", `body` = '" + body + "'";
+					
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+					pstmt.executeUpdate();
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 				lastArticleId++;
-				
+
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
 			} else if (cmd.equals("article list")) {
 				System.out.println("==게시물 목록==");
